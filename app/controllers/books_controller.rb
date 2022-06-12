d634f68ade5ec35de5a436d3c3da55eb077466e6
EC2 Default User <ec2-user@ip-172-31-56-226.ec2.internal>
2022-06-12 15:05:52 +0000

    [Updete]ページ全体の編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,48 @@ class BooksController < ApplicationController
     @book = Book.new 
   end
 
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end 
+    # book.save
+    # redirect_to book_path(book.id)  
+    flash[:notice] = "Book was successfully created." 
+  end
+
   def index
+    @books = Book.all 
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+    flash[:notice] = "Book was successfully update."
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+    flash[:notice] = "Book was successfully destroyed."
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'books/new'
+  # get '/top' => 'homes#top'
+  
+  resources :books
+  
+  root :to => 'homes#top'
+  
+  
+  # post 'books' => 'books#create'
+  # get 'books' => 'books#index'
+  # get 'books/:id' => 'books#show', as: 'book'
+  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  # patch 'books/:id' => 'books#update', as: 'update_book'
+  # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
